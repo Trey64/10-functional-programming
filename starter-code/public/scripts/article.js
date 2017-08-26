a85e0dcb33192552cfa2da1f9c3097069ccac729
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-08-25 17:28:40 -0700

    lab done

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -47,91 +47,103 @@ var app = app || {};
     /* OLD forEach():
     rawData.forEach(function(ele) {
     Article.all.push(new Article(ele));
-  });
-  */
-  Article.all = rows.map(function(ele) {
-    return new Article(ele);
-  });
-};
+    });
+    */
+    Article.all = rows.map(function(ele) {
+      return new Article(ele);
+    });
+  };
 
-Article.fetchAll = callback => {
-  $.get('/articles')
-  .then(
-    results => {
-      Article.loadAll(results);
-      callback();
-    }
-  )
-};
+  Article.fetchAll = callback => {
+    $.get('/articles')
+    .then(
+      results => {
+        Article.loadAll(results);
+        callback();
+      }
+    )
+  };
 
-// TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
-Article.numWordsAll = () => {
-  return Article.all.map().reduce()
-};
+  // DONE: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  Article.numWordsAll = () => {
+    return Article.all.map(article => article.body).reduce((acc, val) => acc + val.split(' ').length, 0);
+  };
 
-// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
-// probably need to use the optional accumulator argument in your reduce call.
-Article.allAuthors = () => {
-  return Article.all.map().reduce();
-};
+  // DONE: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
+  // probably need to use the optional accumulator argument in your reduce call.
 
-Article.numWordsByAuthor = () => {
-  return Article.allAuthors().map(author => {
-    // TODO: Transform each author string into an object with properties for
-    // the author's name, as well as the total number of words across all articles
-    // written by the specified author.
-    // HINT: This .map should be setup to return an object literal with two properties.
-    // The first property should be pretty straightforward, but you will need to chain
-    // some combination of filter, map, and reduce to get the value for the second
-    // property.
+  Article.allAuthors = () => {
+    return Article.all.map(author => author.author).reduce(function(names, name) {
+      if(names.indexOf(name) === -1) {
+        names.push(name);
+      }
+      return names;
+    }, []);
+  };
 
-  })
-};
+  Article.numWordsByAuthor = () => {
+    return Article.allAuthors().map(author => {
 
-Article.truncateTable = callback => {
-  $.ajax({
-    url: '/articles',
-    method: 'DELETE',
-  })
-  .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
-  // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
-  // outside the scope of 301 material, but feel free to research!
-  .then(callback);
-};
+      return {
+        author: author,
+        numWords: Article.all.filter(article => article.author === author).map(words => words.body).reduce((acc, val) => acc + val.split(' ').length, 0)
+      }
 
-Article.prototype.insertRecord = function(callback) {
-  // REVIEW: Why can't we use an arrow function here for .insertRecord()??
-  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-  .then(console.log)
-  .then(callback);
-};
+      // DONE: Transform each author string into an object with properties for
+      // the author's name, as well as the total number of words across all articles
+      // written by the specified author.
+      // HINT: This .map should be setup to return an object literal with two properties.
+      // The first property should be pretty straightforward, but you will need to chain
+      // some combination of filter, map, and reduce to get the value for the second
+      // property.
 
-Article.prototype.deleteRecord = function(callback) {
-  $.ajax({
-    url: `/articles/${this.article_id}`,
-    method: 'DELETE'
-  })
-  .then(console.log)
-  .then(callback);
-};
+    })
+  };
 
-Article.prototype.updateRecord = function(callback) {
-  $.ajax({
-    url: `/articles/${this.article_id}`,
-    method: 'PUT',
-    data: {
-      author: this.author,
-      authorUrl: this.authorUrl,
-      body: this.body,
-      category: this.category,
-      publishedOn: this.publishedOn,
-      title: this.title,
-      author_id: this.author_id
-    }
-  })
-  .then(console.log)
-  .then(callback);
-};
+  Article.truncateTable = callback => {
+    $.ajax({
+      url: '/articles',
+      method: 'DELETE',
+    })
+    .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+    // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
+    // outside the scope of 301 material, but feel free to research!
+    .then(callback);
+  };
+
+  Article.prototype.insertRecord = function(callback) {
+    // REVIEW: Why can't we use an arrow function here for .insertRecord()??
+    $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
+    .then(console.log)
+    .then(callback);
+  };
+
+  Article.prototype.deleteRecord = function(callback) {
+    $.ajax({
+      url: `/articles/${this.article_id}`,
+      method: 'DELETE'
+    })
+    .then(console.log)
+    .then(callback);
+  };
+
+  Article.prototype.updateRecord = function(callback) {
+    $.ajax({
+      url: `/articles/${this.article_id}`,
+      method: 'PUT',
+      data: {
+        author: this.author,
+        authorUrl: this.authorUrl,
+        body: this.body,
+        category: this.category,
+        publishedOn: this.publishedOn,
+        title: this.title,
+        author_id: this.author_id
+      }
+    })
+    .then(console.log)
+    .then(callback);
+  };
 
   module.Article = Article;
 })(app)
